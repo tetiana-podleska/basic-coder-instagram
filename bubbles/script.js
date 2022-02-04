beaa35341951532b4f83076fb97f44cb47a77c1d
Tetiana Podleska <podleskatetiana@gmail.com>
2022-02-04 15:45:36 +0200

    add gitignore

---
+++ bubbles/script.js
@@ -0,0 +1,18 @@
+function createBubbles() {
+    const section = document.querySelector('section');
+
+    const createElement = document.createElement('span');
+
+    let size = Math.random() * 60;
+
+    createElement.style.width = 20 + size + 'px';
+    createElement.style.height = 20 + size + 'px';
+    createElement.style.left = Math.random() * innerWidth + 'px';
+    section.appendChild(createElement);
+
+    setTimeout(() => {
+        createElement.remove()
+    }, 4000)
+}
+
+setInterval(createBubbles, 50);
