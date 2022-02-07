3a682339267680b4daf355e6a64b5993fde7b461
Tetiana Podleska <podleskatetiana@gmail.com>
2022-02-07 15:31:44 +0200

    style / enabled display flex for ul; script / write toggle function apart from draggable

--- draggable navbar/script.js
+++ draggable navbar/script.js
@@ -1,12 +1,18 @@
 const nav = document.querySelector('.nav')
-console.log(nav);
+
+const toggle = document.querySelector('.toggle');
+toggle.onclick = function () {
+    nav.classList.toggle('active')
+}
 
 document.querySelector('.toggle').onClick
     = function () {
-    this.classList.toggle('active');
-    nav.classList.toggle('active');
+    // // this.classList.toggle('active');
+    // nav.classList.toggle('active');
 }
 
 $( function () {
     $('.nav').draggable()
+
 });
+
