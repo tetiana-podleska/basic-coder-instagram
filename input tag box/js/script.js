a8c7ff43ffa688fae6b0478def934d5352259513
Tetiana Podleska <podleskatetiana@gmail.com>
2023-01-04 14:10:42 +0200

    fixed mistakes

--- input tag box/js/script.js
+++ input tag box/js/script.js
@@ -2,6 +2,10 @@ const ul = document.querySelector("ul");
 const input = ul.querySelector("input");
 const countNum = document.querySelector(".details span");
 
+console.log(ul);
+console.log(input);
+console.log(countNum);
+
 let tags = [];
 let maxTags = 10;
 
@@ -19,6 +23,39 @@ function createTag() {
     .reverse()
     .forEach((tag) => {
       console.log(tag);
-      let liTag = `<li>${tag}</li>`;
+      let liTag = `<li>${tag} <i class="fa fa-times" onclick="remove(this, '${tag}')"></i></li>`;
+      ul.insertAdjacentHTML("afterbegin", liTag);
+      countTag();
     });
 }
+
+function remove(element, tag) {
+  let index = tags.indexOf(tag);
+  tags = [...tags.slice(0, index, ...tags.slice(index + 1))];
+  element.parentElement.remove();
+  countTag();
+}
+
+function addTag(e) {
+  if (e.key == "Enter") {
+    let tag = e.target.value.replace(/\s+/g, "");
+    if (tag.length > 1 && !tags.includes(tag)) {
+      if (tags.length < 10) {
+        tag.split(",").forEach((tag) => {
+          tags.push(tag);
+          createTag();
+        });
+      }
+    }
+    e.target.value = "";
+  }
+}
+
+input.addEventListener("keyup", addTag);
+
+const removeBtn = document.querySelector("button");
+removeBtn.addEventListener("click", () => {
+  tags.length = 0;
+  ul.querySelectorAll("li").forEach((li) => li.remove());
+  countTag();
+});
